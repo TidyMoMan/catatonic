7fc426ef7e54d189ec49adae9be9bf3b81e6cfdd
TidyMoMan <mosesdilts@gmail.com>
2025-11-30 12:12:40 -0500

    hexagons hard

---
+++ Board.py
@@ -0,0 +1,9 @@
+from Tile import Tile
+
+class Board:
+    #class attributes
+    map = []
+
+
+    def __init__(self, map, boardstate):
+        pass
---
+++ Tile.py
@@ -0,0 +1,8 @@
+from enum import Enum
+class Tile(Enum):
+    desert = 0
+    forest = 1
+    brickyard = 2
+    mountain = 3
+    pasture = 4
+    farmland = 5
